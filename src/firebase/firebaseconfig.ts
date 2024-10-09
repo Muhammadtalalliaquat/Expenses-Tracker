a5212db5c71588ac1a335faa9e506a0510964264
Muhammad Talal Liaquat <160119412+Muhammadtalalliaquat@users.noreply.github.com>
2024-10-09 15:08:25 +0500

    Update firebaseconfig.ts

--- src/firebase/firebaseconfig.ts
+++ src/firebase/firebaseconfig.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-    NEXT_PUBLIC_apiKey= "AIzaSyC7pK4bURWr3Y8YGXlYGOxhEQGmBLuxLd4"
-    NEXT_PUBLIC_authDomain= "authentication-project-a71a9.firebaseapp.com"
-    NEXT_PUBLIC_projectId= "authentication-project-a71a9"
-    NEXT_PUBLIC_storageBucket= "authentication-project-a71a9.appspot.com"
-    NEXT_PUBLIC_messagingSenderId= "465429438842"
-    NEXT_PUBLIC_appId= "1:465429438842:web:08e663796281fda2bdf6c3"
-    NEXT_PUBLIC_measurementId= "G-FTF8W9KHYS"
+    apiKey= "AIzaSyC7pK4bURWr3Y8YGXlYGOxhEQGmBLuxLd4"
+    authDomain= "authentication-project-a71a9.firebaseapp.com"
+    projectId= "authentication-project-a71a9"
+    storageBucket= "authentication-project-a71a9.appspot.com"
+    messagingSenderId= "465429438842"
+    appId= "1:465429438842:web:08e663796281fda2bdf6c3"
+    measurementId= "G-FTF8W9KHYS"
   };
   
   export const app = initializeApp(firebaseConfig);
