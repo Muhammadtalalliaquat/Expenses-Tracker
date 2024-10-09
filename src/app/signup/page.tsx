bec8aff77aaed2f6bccc6f0ec494b17100518652
Muhammad Talal Liaquat <160119412+Muhammadtalalliaquat@users.noreply.github.com>
2024-10-09 14:58:29 +0500

    Update page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
         showText={"Already have an account? "}
         loginSignupText={"Login"}
         showMsg={"/login"}
+        fogotPassword={""}
         /> 
     </>
   );
